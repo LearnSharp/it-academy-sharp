9f1aa7e274b1526d51eb542d9f59d5b5c0aa7536
Andrei Metelski <8metya8@gmail.com>
2019-01-20 21:11:19 +0300

    Added Homewrork03_optional

---
+++ 14/Homework03_optional/Homework03_optional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework03_optional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int result;
+
+            while(n <= 10)
+            {
+                result = 1;
+
+                for(int i = 0; i < n; i++)
+                {
+                    result *= 2;
+                }
+
+                Console.WriteLine($"2 to the power {n} - {result}");
+                n++;
+            }
+
+            Console.WriteLine("Press any key for exit.");
+            Console.ReadKey();
+        }
+    }
+}
