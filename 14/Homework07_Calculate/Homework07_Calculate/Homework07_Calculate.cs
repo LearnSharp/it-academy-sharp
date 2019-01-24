14ef0bd88b55ce7746ce9e182f71afde2579279a
Andrei Metelski <8metya8@gmail.com>
2019-01-24 17:15:17 +0300

    Added homework07

---
+++ 14/Homework07_Calculate/Homework07_Calculate/Homework07_Calculate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework07_Calculate
+{
+    class Homework07_Calculate
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Enter first argument. value should be >= 0");
+            while (!Int32.TryParse(Console.ReadLine(), out a) || (a < 0))
+            {
+                Console.WriteLine("Entered value is not valid. Try again.");
+                Console.WriteLine("Enter first argument. value should be >= 0");
+            }
+
+            Console.WriteLine("Enter second argument. value should be >= 0");
+            while (!Int32.TryParse(Console.ReadLine(), out b) || (b < 0))
+            {
+                Console.WriteLine("Entered value is not valid. Try again.");
+                Console.WriteLine("Enter second argument. value should be >= 0");
+            }
+
+            Console.WriteLine("Enter third argument. value should be >= 0");
+            while (!Int32.TryParse(Console.ReadLine(), out c) || (c < 0))
+            {
+                Console.WriteLine("Entered value is not valid. Try again.");
+                Console.WriteLine("Enter third argument. value should be >= 0");
+            }
+
+            Calculate(a, b, c);
+
+            Console.ReadKey();
+        }
+
+        public static void Calculate(int argument_1, int argument_2, int argument_3)
+        {
+            Console.WriteLine("Arithmetic mean value - {0}", Math.Round((argument_1 + argument_2 + argument_3) / 3.0, 2));
+        }
+    }
+}
---
+++ 14/Homework07_FxConverter/Homework07_FxConverter/Homework07_FxConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework07_FxConverter
+{
+    class Homework07_FxConverter
+    {
+        static void Main(string[] args)
+        {
+           decimal _amount;
+           decimal _rate;
+
+            Console.WriteLine("Enter amount for covert: ");
+            while(!Decimal.TryParse(Console.ReadLine(), out _amount) || _amount <= 0)
+            {
+                Console.WriteLine("Entered amount is not valid. Try again.");
+                Console.WriteLine("Enter amount for covert: ");
+            }
+
+            Console.WriteLine("Enter rate for covert: ");
+            while (!Decimal.TryParse(Console.ReadLine(), out _rate) || _rate <= 0)
+            {
+                Console.WriteLine("Entered rate is not valid. Try again.");
+                Console.WriteLine("Enter rate for covert: ");
+            }
+
+            Console.WriteLine("You can buy {0} units some curency", FxConverter(_amount, _rate));
+
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Press any key for exit.");
+            Console.ReadKey();
+        }
+
+        public static decimal FxConverter(decimal amount, decimal rate)
+        {
+            return Math.Round(amount / rate, 2);
+        }
+
+    }
+}
