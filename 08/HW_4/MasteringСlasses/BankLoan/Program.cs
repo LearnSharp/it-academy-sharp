2718c4ea3a180d9aa4f5a4c8f5aac517a0b19665
Sergiro Z <zujonok@hotmail.com>
2019-01-25 19:14:09 +0300

    deploy

--- 08/HW_4/MasteringСlasses/BankLoan/Program.cs
+++ 08/HW_4/MasteringСlasses/BankLoan/Program.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-
 namespace BankLoan
 {
     internal class Program
     {
+        private static void Main()
+        {
+            var customer = new Customer();
+            customer.Name = "John Doe";
+
+            var receivedLoan = new ReceivedLoan
+            {
+                DateOfReceiving = Convert.ToDateTime("01.01.2020"),
+                TotalLoan = 1000
+            };
+
+            var payment = new PaymentLoan();
+
+            payment.AddPayment("17.01.2020", 100);
+            payment.AddPayment("17.01.2020", 200);
+            payment.AddPayment("18.01.2020", 300);
+            payment.AddPayment("19.01.2020", 400);
+            payment.AddPayment("18.01.2020", 50);
+
+            customer.MessageStatement(receivedLoan, payment);
+            Console.WriteLine();
+        }
+
+        //*****************************************************
+
         public class ReceivedLoan
         {
             public DateTime DateOfReceiving { get; set; }
@@ -16,18 +40,22 @@ namespace BankLoan
 
         public class PaymentLoan
         {
-            internal struct SeparatePayment
-            {
-                public DateTime DateOfPayment { get; internal set; }
-                public decimal SumPayment { get; internal set; }
-            }
-
-            private List<SeparatePayment> PaymentList { get; }
-
             public PaymentLoan()
             {
                 var pmtList = new List<SeparatePayment>();
                 PaymentList = pmtList;
+            }
+
+            private List<SeparatePayment> PaymentList { get; }
+
+            public decimal TotalPayment
+            {
+                get
+                {
+                    if (PaymentList.Count == 0) return 0;
+                    var total = PaymentList.Sum(x => x.SumPayment);
+                    return total;
+                }
             }
 
             public void AddPayment(string strDate, decimal sumPayment)
@@ -64,20 +92,18 @@ namespace BankLoan
                                TotalPayment);
             }
 
-            public decimal TotalPayment
+            private static string MessageOfPayment(
+                string strDate, decimal sumPayment)
             {
-                get
-                {
-                    if (PaymentList.Count == 0) return 0;
-                    var total = PaymentList.Sum(x => x.SumPayment);
-                    return total;
-                }
+                return strDate + " you payment in the amount of " + sumPayment +
+                       " was made.";
             }
 
-            private static string MessageOfPayment(
-                string strDate, decimal sumPayment) =>
-                strDate + " you payment in the amount of " + sumPayment +
-                " was made.";
+            internal struct SeparatePayment
+            {
+                public DateTime DateOfPayment { get; internal set; }
+                public decimal SumPayment { get; internal set; }
+            }
         }
 
         internal class Customer
@@ -85,8 +111,10 @@ namespace BankLoan
             public string Name { get; set; }
 
             public decimal
-                GetBalance(ReceivedLoan amount, PaymentLoan pmtLoan) =>
-                amount.TotalLoan - pmtLoan.TotalPayment;
+                GetBalance(ReceivedLoan amount, PaymentLoan pmtLoan)
+            {
+                return amount.TotalLoan - pmtLoan.TotalPayment;
+            }
 
             public void MessageStatement(ReceivedLoan amount,
                                          PaymentLoan pmtLoan)
@@ -95,9 +123,9 @@ namespace BankLoan
                                   amount.TotalLoan, amount.DateOfReceiving);
                 Console.WriteLine(string.Concat(Enumerable.Repeat("*", 15)));
                 pmtLoan.PrintListOfPayment();
-                var balance = amount.TotalLoan - pmtLoan.TotalPayment;
+                var balance = GetBalance(amount, pmtLoan);
                 var output = "The balance of your account as of " +
-                             DateTime.Now.ToString("dd MMMM yyyy | HH:mm:ss") +
+                             DateTime.Now.ToString("dd MMMM yyyy (HH:mm:ss)") +
                              " is: " +
                              balance.ToString(CultureInfo.InvariantCulture) +
                              "=";
@@ -106,44 +134,15 @@ namespace BankLoan
                                                                       .Length)));
                 Console.WriteLine(output);
                 if (balance > 0)
-                {
                     Console.WriteLine("You must bank {0} =", balance);
-                }
                 else if (balance < 0)
-                {
                     Console.WriteLine("The bank has a debt of {0} to you =",
                                       -balance);
-                }
                 else if (balance == 0)
-                {
                     Console.WriteLine("Your account balance is 0 =. " +
                                       "There are no mutual obligations between " +
                                       "you and the bank.");
-                }
             }
-        }
-
-        private static void Main()
-        {
-            var customer = new Customer();
-            customer.Name = "John Doe";
-
-            var receivedLoan = new ReceivedLoan
-            {
-                DateOfReceiving = Convert.ToDateTime("01.01.2020"),
-                TotalLoan = 1000
-            };
-
-            var payment = new PaymentLoan();
-
-            payment.AddPayment("17.01.2020", 100);
-            payment.AddPayment("17.01.2020", 200);
-            payment.AddPayment("18.01.2020", 300);
-            payment.AddPayment("19.01.2020", 400);
-            payment.AddPayment("18.01.2020", 50);
-
-            customer.MessageStatement(receivedLoan, payment);
-            Console.WriteLine();
         }
     }
 }
---
+++ 08/HW_4/MasteringСlasses/CurrencyConversion/Converter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CurrencyConversion
+{
+    internal class Converter
+    {
+        private static decimal SumConv;
+        private static decimal ConvRate;
+
+        /// <summary>
+        ///     Вводимые значения ограничиваются только цифрами
+        /// </summary>
+        /// <param name="str">число в строковом виде</param>
+        /// <param name="decimalVar">результат тип decimal</param>
+        private static void EnterDecimal(string str, out decimal decimalVar)
+        {
+            while (true)
+            {
+                if (str != null && Regex.IsMatch(str, "^\\d+(\\.\\d+)?$"))
+                {
+                    str = str.Replace('.', ',');
+                    decimal.TryParse(str, out decimalVar);
+                }
+                else
+                {
+                    Console.WriteLine("Error input! Please repeat enter: ");
+                    str = Console.ReadLine();
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        /// <summary>
+        ///     Представление ввода
+        /// </summary>
+        private static void InputView()
+        {
+            Console.WriteLine("Enter the amount to convert (rub): ");
+            var str = Console.ReadLine();
+            EnterDecimal(str, out SumConv);
+
+            Console.WriteLine("Enter the conversion rate (rub/usd): ");
+            str = Console.ReadLine();
+            EnterDecimal(str, out ConvRate);
+        }
+
+        private static void GetResult(out decimal result)
+        {
+            result = SumConv / ConvRate;
+        }
+
+        /// <summary>
+        ///     Представление вывода
+        /// </summary>
+        private static void OutputView()
+        {
+            Console.WriteLine("\nCarrying out the conversion of rubles to usd.");
+            GetResult(out var result);
+            Console.WriteLine("The total amount received after the conversion will be:: {0:f2} usd",
+                              result);
+        }
+
+        private static void Main(string[] args)
+        {
+            InputView();
+            OutputView();
+        }
+    }
+}
