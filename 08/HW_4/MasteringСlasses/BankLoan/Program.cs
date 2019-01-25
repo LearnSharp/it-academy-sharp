e29deb23a0936f6281d312b3da89a6ff0cbb9c2f
Sergiro Z <zujonok@hotmail.com>
2019-01-25 12:44:42 +0300

    BankLoan 1ed.

---
+++ 08/HW_4/MasteringСlasses/BankLoan/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+
+namespace BankLoan
+{
+    internal class Program
+    {
+        public class ReceivedLoan
+        {
+            public DateTime DateOfReceiving { get; set; }
+            public decimal TotalLoan { get; set; }
+        }
+
+        public class PaymentLoan
+        {
+            public PaymentLoan(DateTime dateOfPayment, decimal sumPayment)
+            {
+                DateOfPayment = dateOfPayment;
+                SumPayment = sumPayment;
+            }
+
+            public DateTime DateOfPayment { get; set; }
+            public decimal SumPayment { get; set; }
+        }
+
+        internal class Customer
+        {
+            public string Name { get; set; }
+
+            public List<PaymentLoan> PaymentList { get; set; }
+
+            public string RepaymentService(decimal amount, decimal payment)
+            {
+                var total = PaymentList.Sum(x => x.SumPayment);
+                total += payment;
+                var balance = amount - total;
+                var str = balance.ToString(CultureInfo.InvariantCulture);
+
+                return str;
+            }
+        }
+
+        private static void Main()
+        {
+            var customer = new Customer
+            {
+                Name = "John Doe",
+                PaymentList = new List<PaymentLoan>(),
+            };
+
+            var receivedLoan = new ReceivedLoan
+            {
+                DateOfReceiving = Convert.ToDateTime("01.01.2020"),
+                TotalLoan = 1000,
+            };
+
+            var paymentLoan =
+                new PaymentLoan(Convert.ToDateTime("17.01.2020"), 100);
+            customer.PaymentList.Add(paymentLoan);
+
+            var paymentLoan1 =
+                new PaymentLoan(Convert.ToDateTime("19.01.2020"), 200);
+            customer.PaymentList.Add(paymentLoan1);
+
+            var str = customer.RepaymentService(receivedLoan.TotalLoan, 50);
+
+            Console.WriteLine("result = " + str);
+        }
+    }
+}
