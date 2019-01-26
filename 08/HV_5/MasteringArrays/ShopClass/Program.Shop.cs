5655fad32eeaad19eba01004a2f10e284d1a0bd9
Sergiro Z <zujonok@hotmail.com>
2019-01-27 02:39:21 +0300

    Shop deploy

---
+++ 08/HV_5/MasteringArrays/ShopClass/Program.Shop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShopClass
+{
+    internal partial class Program
+    {
+        private class Shop
+        {
+            public Shop()
+            {
+                var articles = new List<Article>();
+                Articles = articles;
+            }
+
+            private List<Article> Articles { get; }
+
+            public ArrayList GetArticleByIndex(int index)
+            {
+                var fnd = Articles.ElementAtOrDefault(index);
+                return fnd?.GetArticleView();
+            }
+
+            public ArrayList GetArticleByName(string name)
+            {
+                foreach (var itm in Articles)
+                    if (itm.GetArticleView().Contains(name))
+                        return itm.GetArticleView();
+
+                return null;
+            }
+
+            public void AddArticles(Article article)
+            {
+                Articles.Add(article);
+            }
+
+            public void GetArticles()
+            {
+                if (!Articles.Any()) return;
+                Console.WriteLine("Introduced articles:\n"
+                                      .PadRight(20, '\u2500'));
+                var num = 0;
+                foreach (var it in Articles)
+                {
+                    Console.Write("{0}  ", ++num);
+                    foreach (var i in it.GetArticleView())
+                        Console.Write("{0} ", i);
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\n".PadRight(20, '\u2500'));
+            }
+        }
+    }
+}
---
+++ 08/HV_5/MasteringArrays/ShopClass/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+
+namespace ShopClass
+{
+    internal partial class Program
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static void ShopAllView(Shop shop)
+        {
+            Console.WriteLine("\n".PadRight(20, '\u2500'));
+            shop.GetArticles();
+        }
+
+        private static void GetItemView(IEnumerable fnd)
+        {
+            if (fnd == null) return;
+            Console.WriteLine();
+            foreach (var item in fnd) Console.Write("{0} ", item);
+
+            Console.WriteLine();
+        }
+
+        private static void ShopFindByIndex(Shop shop)
+        {
+            Console.Write("Enter the product Index in the store list: ");
+            EnterDecimal(Console.ReadLine(), out var num);
+            var fnd = shop.GetArticleByIndex(Convert.ToInt16(num));
+            if (fnd != null) GetItemView(fnd);
+            else Console.WriteLine("Search returned no results.");
+        }
+
+        private static void ShopFindByName(Shop shop)
+        {
+            Console.Write("Enter the product Name in the store list: ");
+            var fnd = shop.GetArticleByName(Console.ReadLine());
+            if (fnd != null) GetItemView(fnd);
+            else Console.WriteLine("Search returned no results.");
+        }
+
+        private static void TradeController()
+        {
+            var shop = new Shop();
+            ConsoleKeyInfo inp;
+            do
+            {
+                var article = new Article();
+                article.CreateArticle();
+                shop.AddArticles(article);
+                Console.WriteLine("\nContinue entering?\nPress any key " +
+                                  "for continue or Esc for exit.\n");
+                inp = Console.ReadKey(true);
+            } while (inp.Key != ConsoleKey.Escape);
+
+
+            //ShopAllView(shop);
+            ShopFindByIndex(shop);
+            ShopFindByName(shop);
+        }
+
+        /// <summary>
+        ///     • display information about the product by number using the index;
+        ///     • display information about the product, the name of which is entered
+        ///     from the keyboard, if there are no such goods, to issue a
+        ///     corresponding message;
+        /// </summary>
+        private static void Main()
+        {
+            TradeController();
+        }
+    }
+}
