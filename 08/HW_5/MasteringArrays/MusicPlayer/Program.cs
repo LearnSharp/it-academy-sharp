48c1530ccb4a5f130de250e85ecc8efda57e80ca
Sergiro Z <zujonok@hotmail.com>
2019-01-27 21:35:22 +0300

    edd menu

--- 08/HW_5/MasteringArrays/MusicPlayer/Program.cs
+++ 08/HW_5/MasteringArrays/MusicPlayer/Program.cs
@@ -57,7 +57,8 @@ namespace MusicPlayer
             KeyValuePair<int, MenuItem> selectedMenu);
     }
 
-    public class MenuState1 : MenuState
+
+    public class MenuStateGlobal : MenuState
     {
         /// <summary>
         /// Creating list of menu
@@ -65,10 +66,11 @@ namespace MusicPlayer
         protected override Dictionary<int, MenuItem> Menus { get; } =
             new Dictionary<int, MenuItem>
             {
-                {1, new MenuItem {Text = "Menu 1"}},
-                {2, new MenuItem {Text = "Menu 2"}},
-                {3, new MenuItem {Text = "Menu 3"}},
-                {4, new MenuItem {Text = "Exit"}}
+                {1, new MenuItem {Text = "Previous"}},
+                {2, new MenuItem {Text = "Play"}},
+                {3, new MenuItem {Text = "Next"}},
+                {4, new MenuItem {Text = "Play List"}},
+                {5, new MenuItem {Text = "Exit"}}
             };
 
 
@@ -80,27 +82,146 @@ namespace MusicPlayer
         protected override IState NextState(
             KeyValuePair<int, MenuItem> selectedMenu)
         {
-            if (selectedMenu.Key == 4) return null;
-            if (selectedMenu.Key == 1) return new AuthState();
+            if (selectedMenu.Key == 5) return null;
+            if (selectedMenu.Key == 1) return new PreviousState();
+            if (selectedMenu.Key == 2) return new PlayState();
+            if (selectedMenu.Key == 3) return new NextState();
+            if (selectedMenu.Key == 4) return new MenuPlayList();
+
             return this;
         }
     }
 
-    public class AuthState : IState
+    public class PreviousState : IState
     {
         IState IState.RunState()
         {
-            Console.WriteLine("Login: ");
-            var login = Console.ReadLine();
-            Console.WriteLine("Password: ");
-            // ReSharper disable once UnusedVariable
-            var password = Console.ReadLine();
-
-            Console.WriteLine($"Hello, {login}");
+            Console.WriteLine("***PreviousState: ***");
 
             Console.ReadLine();
             Console.Clear();
-            return new MenuState1();
+            return new MenuStateGlobal();
+        }
+    }
+
+    public class PlayState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***PlayState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuStateGlobal();
+        }
+    }
+
+    public class NextState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***NextState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuStateGlobal();
+        }
+    }
+
+
+    public class MenuPlayList : MenuState
+    {
+        /// <summary>
+        /// Creating list of menu
+        /// </summary>
+        protected override Dictionary<int, MenuItem> Menus { get; } =
+            new Dictionary<int, MenuItem>
+            {
+                {1, new MenuItem {Text = "Add"}},
+                {2, new MenuItem {Text = "Delete"}},
+                {3, new MenuItem {Text = "Play list forward"}},
+                {4, new MenuItem {Text = "Play list back"}},
+                {5, new MenuItem {Text = "Play list random"}},
+                {6, new MenuItem {Text = "Exit"}}
+            };
+
+
+        /// <summary>
+        /// Actions when selecting a menu item with a specific number
+        /// </summary>
+        /// <param name="selectedMenu">Specific number point menu</param>
+        /// <returns></returns>
+        protected override IState NextState(
+            KeyValuePair<int, MenuItem> selectedMenu)
+        {
+            if (selectedMenu.Key == 6) return new MenuStateGlobal();
+            if (selectedMenu.Key == 1) return new AddState();
+            if (selectedMenu.Key == 2) return new DeleteState();
+            if (selectedMenu.Key == 3) return new ForwardState();
+            if (selectedMenu.Key == 4) return new BackState();
+            if (selectedMenu.Key == 5) return new RandomState();
+
+            return new MenuStateGlobal();
+        }
+    }
+
+    public class AddState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***AddState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuPlayList();
+        }
+    }
+
+    public class DeleteState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***DeleteState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuPlayList();
+        }
+    }
+
+    public class ForwardState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***ForwardState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuPlayList();
+        }
+    }
+
+    public class BackState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***BackState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuPlayList();
+        }
+    }
+
+    public class RandomState : IState
+    {
+        IState IState.RunState()
+        {
+            Console.WriteLine("***RandomState: ***");
+
+            Console.ReadLine();
+            Console.Clear();
+            return new MenuPlayList();
         }
     }
 
@@ -127,30 +248,29 @@ namespace MusicPlayer
         }
     }
 
-
     internal class Program
     {
+        //private static void Test()
+        //{
+        //    var menuState = new ConfigurableMenuState();
+        //    menuState.AddMenuItem(1, new MenuItem {Text = "Option 1"},
+        //                          () => new AuthState());
+        //    menuState.AddMenuItem(2, new MenuItem {Text = "Exit"},
+        //                          () => null);
+
+        //    IState startState = menuState;
+        //    while (startState != null) startState = startState.RunState();
+        //}
+
+        //private static void Test1()
+        //{
+        //    IState startState = new AuthState();
+        //    while (startState != null) startState = startState.RunState();
+        //}
+
         private static void Test()
         {
-            var menuState = new ConfigurableMenuState();
-            menuState.AddMenuItem(1, new MenuItem {Text = "Option 1"},
-                                  () => new AuthState());
-            menuState.AddMenuItem(2, new MenuItem {Text = "Exit"},
-                                  () => null);
-
-            IState startState = menuState;
-            while (startState != null) startState = startState.RunState();
-        }
-
-        private static void Test1()
-        {
-            IState startState = new AuthState();
-            while (startState != null) startState = startState.RunState();
-        }
-
-        private static void Test2()
-        {
-            IState startState = new MenuState1();
+            IState startState = new MenuStateGlobal();
             while (startState != null) startState = startState.RunState();
         }
 
