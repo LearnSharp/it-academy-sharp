63b9cf789964256e8d7e666393f77bf7aedbab25
Sergiro Z <zujonok@hotmail.com>
2019-02-05 04:17:23 +0300

    Bug fixes and minor improvements

--- 08/HW_5/MasteringArrays/MusicPlayer/Models/RandomState.cs
+++ 08/HW_5/MasteringArrays/MusicPlayer/Models/RandomState.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
 using MusicPlayer.Interface;
 using static MusicPlayer.Program;
 
@@ -15,10 +14,11 @@ namespace MusicPlayer.Models
             HandlerEsc handlerEsc = HandLoopForward;
             HeaderState(CurrentPlaylist, StRandom);
 
+            Task.Factory.StartNew(InterruptEndExecuteFindSong);
             handlerEsc();
 
             FooterState();
-            //Task.Factory.StartNew(InterruptEndExecuteFindSong);
+            //
             return new MenuPlay();
         }
 
@@ -30,26 +30,16 @@ namespace MusicPlayer.Models
 
         private static void InterruptEndExecuteFindSong()
         {
-            //var cki = Console.ReadKey(true);
-            //if (cki.Key== ConsoleKey.F)
-            //{
-            //    while (Console.KeyAvailable) Console.ReadKey(false);
+            var cki = Console.ReadKey(true);
+            if (cki.Key == ConsoleKey.F)
+            {
+                while (Console.KeyAvailable) Console.ReadKey(false);
 
-            //    Console.Clear();
-            //    Console.WriteLine("******** Method Find **********");
+                Console.Clear();
+                Console.WriteLine("******** Method Find **********");
 
-            //    var tmpArray = PlaySonglist.GetSongByIndex(0).ToArray();
-            //    var songTitle = tmpArray.GetValue(0).ToString();
-            //    var timeSong = tmpArray.GetValue(1).ToString();
-            //    var authSong = tmpArray.GetValue(2).ToString();
-            //    var tmpSong = new Song(songTitle, timeSong, authSong);
-            //    PlaySonglist.AddAtIndexSongPlaylist(IndexOfSong, tmpSong);
-            //    ControlEvent = true;
-            //    foreach (var pos in CountOfSong)
-            //        SongHandler(PlaySonglist, pos);
-            //}
-            //Console.Clear();
-            Console.WriteLine("\n**************************");
+                ControlEvent = true;
+            }
         }
     }
 }
