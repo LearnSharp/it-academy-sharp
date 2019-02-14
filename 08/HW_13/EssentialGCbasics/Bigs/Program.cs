95dda60c91e3a34073c4e9e347108ffa04a5d414
Sergiro Z <zujonok@hotmail.com>
2019-02-14 03:05:08 +0300

    gc

---
+++ 08/HW_13/EssentialGCbasics/Bigs/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Threading;
+
+namespace Bigs
+{
+    internal sealed class BigObject : IDisposable
+    {
+        private readonly List<List<decimal>> _vs;
+
+        public BigObject()
+        {
+            _vs = new List<List<decimal>>();
+            const int MaxCount = 500;
+            const decimal Value = 1000000;
+            for (var i = 0; i < MaxCount; i++)
+            {
+                var tmp = new List<decimal>();
+                for (var j = 0; j < MaxCount; j++) tmp.Add(Value);
+                _vs.Add(tmp);
+            }
+        }
+
+        private bool disposed = false;
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Освобождаем управляемые ресурсы
+                    GC.WaitForPendingFinalizers();
+                    Console.WriteLine("Big Object was destroyed...");
+                }
+
+                // освобождаем неуправляемые объекты
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~BigObject()
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                Console.Write('.');
+                Thread.Sleep(150);
+            }
+
+            Console.WriteLine();
+            Dispose(false);
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var bigObject = new BigObject();
+
+            Console.WriteLine(GC.GetGeneration(bigObject));
+
+            bigObject.Dispose();
+
+            Console.WriteLine(GC.GetGeneration(bigObject));
+
+            using (var bigObject1 = new BigObject())
+            {
+                Console.WriteLine(GC.GetGeneration(bigObject1));
+            }
+
+            var bigObject2 = new BigObject();
+            GC.Collect();
+
+            Console.WriteLine("\nGetGeneration:");
+            Console.WriteLine(GC.GetGeneration(bigObject));
+            Console.WriteLine(GC.GetGeneration(bigObject2));
+
+            Console.WriteLine();
+        }
+    }
+}
